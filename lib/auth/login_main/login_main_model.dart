fc2ef11bc245fbbab1393fe4540862fc8fce5981
m-corderoy <140949202+m-corderoy@users.noreply.github.com>
2026-03-27 14:58:37 +0000

    Remove unnecessary safeSetState calls
    Removed redundant safeSetState calls after dispatching UI control.

--- lib/auth/login_main/login_main_model.dart
+++ lib/auth/login_main/login_main_model.dart
@@ -130,14 +130,12 @@ class LoginMainModel extends FlutterFlowModel<LoginMainWidget> {
                       teamTreeIDStart: teamTree,
                       roleTreeIDStart: roleTree,
                     );
-                    safeSetState(() {});
                     // set params dispatch  view
                     FFAppState().dispatchUIcontrol = DispatchUIStruct(
                       selectedLocation:
                           FFAppState().session.locationTreeIDStart,
                       selectedTeam: FFAppState().session.teamTreeIDStart,
                     );
-                    safeSetState(() {});
                   },
                 ),
               ),
